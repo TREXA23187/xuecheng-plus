bf3211575c9cae849d2ec5ea0946615e2a178fe6
TREXA23187 <498111557@qq.com>
2023-02-16 10:56:10 +0000

    fix: spring boot version

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApiApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableSwagger2Doc
 public class ContentApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ContentApiApplication.class, args);
 	}
